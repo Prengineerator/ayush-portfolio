a0f886331deb8fed2211df566fbd2349a14bd6c2
siddharth195 <siddharthmalhotra147@gmail.com>
2024-03-03 15:03:13 +0530

    updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,45 @@ const tabs = document.querySelectorAll('[data-target]'),
 
 
 /*=============== CONTACT FORM =============== */
+
+const contactForm= document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail= document.getElementById('contact-email'),
+    contactSubject= document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (
+        contactName.value === '' ||
+        contactEmail.value === '' ||
+        contactSubject.value === '' ||
+        contactMessage.value === ''
+    )   {
+        errorMessage.textContent = 'Write all the input fields'
+    }
+    else {
+        emailjs.sendForm('service_5tjqfph','template_b54xe3l','#contact-form','nK732mfKmCJbU0Dhl')
+        .then(()=>{
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Message Sent ✓';
+
+            //remove message after 5 seconds 
+            setTimeout(()=>{
+                errorMessage.textContent = '';
+            },5000);
+        }, (error) => { alert('OOPs! Something Went Wrong...', error) }
+        
+        );
+
+        contactName.value = '';
+        contactEmail.value = '';
+        contactSubject.value ='';
+        contactMessage.value ='';
+    }
+};
+
+contactForm.addEventListener('submit',sendEmail);
+
